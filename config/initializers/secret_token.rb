0df4a31ed403ecaee27e32e62d6d0eb825f3ea56
ablechman <andrew.n.blechman@gmail.com>
2013-10-17 15:25:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Farmshare::Application.config.secret_key_base = 'cbd4cb3657e34db1936fd2b3e2d8e0260884da554f7fd22cdf68a5899fbc1e21e578d081e0e91d755b9de27abd3235d0e7757366a6cf1cb2a273cea32c28d734'
