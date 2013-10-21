accd143208e79bb0daa156e84db9420be87a54a8
ablechman <andrew.n.blechman@gmail.com>
2013-10-21 11:30:33 -0400

    Added home and buy pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Farmshare::Application.routes.draw do
+  root "pages#home"
+  get "buy" => "pages#buy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
