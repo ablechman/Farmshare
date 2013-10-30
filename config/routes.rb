82f8b90b1168873bbe3528e6140d30a90fcdba75
ablechman <andrew.n.blechman@gmail.com>
2013-10-30 16:01:46 -0400

    added sell page and relevant links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Farmshare::Application.routes.draw do
+  devise_for :admins
   devise_for :users
   root "pages#home"
   get "buy" => "pages#buy"
+  get "sell" => "pages#sell"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
